0ce8c2d69b4a8987f4f6ff37c147f2f8af3fa594
tabuna <bliz48rus@gmail.com>
2022-04-01 21:56:23 +0300

    Update assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,6 @@
 import { Application } from '@hotwired/stimulus';
 import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
 
-
-require('bootstrap/dist/js/bootstrap.bundle')
-
-
 const application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 application.load(definitionsFromContext(context));
