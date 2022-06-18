9816cfd9cdc4738ff49c0c2abb30085e5c0fa5a2
tabuna <bliz48rus@gmail.com>
2022-06-18 17:16:11 +0300

    Added turbo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
+import * as Turbo from "@hotwired/turbo"
 import { Application } from '@hotwired/stimulus';
 import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
 
 const application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+
+window.Turbo = Turbo;
