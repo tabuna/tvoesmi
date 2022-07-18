cd20206a42db0c13f7420f554290e133b826df69
tabuna <bliz48rus@gmail.com>
2022-07-18 03:30:40 +0300

    Build assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
-import * as Turbo from "@hotwired/turbo"
-import { Application } from '@hotwired/stimulus';
-import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
+import * as Turbo from '@hotwired/turbo';
 
-const application = Application.start();
+import {Application}            from '@hotwired/stimulus';
+import {definitionsFromContext} from '@hotwired/stimulus-webpack-helpers';
+
+const application = window.application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 application.load(definitionsFromContext(context));
 
