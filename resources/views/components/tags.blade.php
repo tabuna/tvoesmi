28fdcdd08cec3d6067e39856a96b60f05f2656f7
tabuna <bliz48rus@gmail.com>
2022-07-26 19:56:34 +0300

    Change decorate tags links

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -5,13 +5,13 @@
             @if(\Illuminate\Support\Str::endsWith(url()->current(), $tag['slug']))
                 <a href="/list"
                    data-turbo-action="replace"
-                   class="badge bg-success border">
+                   class="badge bg-success border text-decoration-none">
                     {{ $tag['name'] }}
                 </a>
             @else
                 <a href="/tags/{{ $tag['slug'] }}"
                    data-turbo-action="replace"
-                   class="badge bg-secondary border">
+                   class="badge bg-secondary border text-decoration-none">
                     {{ $tag['name'] }}
                 </a>
             @endif
