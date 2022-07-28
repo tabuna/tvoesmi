2340143f8372559cd1eed311f11f6a9381fca8c1
Alexandr Chernyaev <bliz48rus@gmail.com>
2022-07-28 21:34:31 +0300

    Update html.blade.php

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
     <title>@yield('title','Твоё СМИ - Новостной агрегатор')</title>
     <meta name="description"
           content="@yield('description','Самые горячие новости в России, в США, в мире. Последние события в мире новостей.')">
