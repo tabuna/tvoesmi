f148ea3aa7a59cbcc06047f9775272814f5a5b8b
Alexandr Chernyaev <bliz48rus@gmail.com>
2022-08-03 17:02:45 +0300

    Update smi.php

--- config/smi.php
+++ config/smi.php
@@ -72,39 +72,6 @@ return [
     */
 
     'rss' => [
-        'https://www.watson.ch/api/1.0/rss/index.xml',
-        'https://www.srf.ch/news/bnf/rss/1646',
-        'https://www.nzz.ch/recent.rss',
-        'https://www.blick.ch/news/rss.xml',
-        'https://partner-feeds.publishing.tamedia.ch/rss/tagesanzeiger/ticker',
-        'https://bazonline.ch/basel/rss.html',
-        'https://partner-feeds.publishing.tamedia.ch/rss/derbund/ticker',
-        'https://partner-feeds.20min.ch/rss/20minuten',
-        'https://partner-feeds.publishing.tamedia.ch/rss/bazonline/ticker',
-        'https://www.faz.net/rss/aktuell/',
-        'https://www.srf.ch/news/bnf/rss/1890',
-        'https://telebasel.ch/feed/',
-        'https://www.watson.ch/api/1.0/rss/index.xml?tag=Schweiz',
-        'https://newsfeed.zeit.de/index',
-        'https://rss.sueddeutsche.de/app/service/rss/alles/index.rss?output=rss',
-        'https://partner-feeds.publishing.tamedia.ch/rss/bernerzeitung/ticker',
-        'https://www.luzernerzeitung.ch/schweiz.rss',
-        'https://www.tagblatt.ch/schweiz.rss',
-        'https://www.persoenlich.com/rss/news.xml',
-        'https://insideparadeplatz.ch/feed/',
-        'http://www.news.ch/rss/letztenews.xml',
-        'https://www.netzwoche.ch/taxonomy/term/31/feed',
-        'https://t3n.de/rss.xml',
-        'https://rss.golem.de/rss.php?feed=RSS2.0',
-        'https://www.heise.de/rss/heise-atom.xml',
-        'https://www.derstandard.at/rss',
-        'https://www.diepresse.com/rss/home',
-        'https://www.kleinezeitung.at/rss/chronik',
-        'https://kurier.at/xml/rssd',
-    ],
-    
-    /*
-    'rss' => [
         // smi
         'https://www.vedomosti.ru/rss/news',
         'https://istories.media/rss/all.xml',
@@ -198,7 +165,6 @@ return [
 
         'https://rss.unian.net/site/news_rus.rss?',
     ],
-*/
     
     /*
     |--------------------------------------------------------------------------
